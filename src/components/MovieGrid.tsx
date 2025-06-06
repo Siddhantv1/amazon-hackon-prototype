23a5a03b1b98b218b89a439f7916db73a6970e5c
Siddhantv1 <siddhaaaaaaant@gmail.com>
2025-06-06 16:04:48 +0530

    change movie poster

--- src/components/MovieGrid.tsx
+++ src/components/MovieGrid.tsx
@@ -1,10 +1,5 @@
 import React from 'react';
 import MoviePoster from "./MoviePoster";
-
-// --- SUGGESTION FOR MORE POSTERS ---
-// To add more variety, move this array into a separate file (e.g., `src/data/movies.ts`)
-// and import it here. Then you can easily add or remove movies without
-// changing the component's code. More posters will improve the randomness.
 const movies = [
   { title: "Prestige", imageUrl: "https://i.pinimg.com/736x/4f/4d/fa/4f4dfabe5c46358c8d821bd2716e8186.jpg" },
   { title: "John Wick 4", imageUrl: "https://cosmicbook.news/wp-content/uploads/2022/11/john-wick-4-poster.jpg" },
@@ -13,13 +8,13 @@ const movies = [
   { title: "Inglourious Basterds", imageUrl: "https://image.tmdb.org/t/p/original/7TdTYRPKf0FlYSbRCZmCoD6jDNX.jpg" },
   { title: "Matrix", imageUrl: "https://movieposterhub.com/cdn/shop/files/japanese_b2_matrix.jpg?v=1707461762" },
   { title: "Interstellar", imageUrl: "https://m.media-amazon.com/images/I/81kz06oSUeL.jpg" },
-  { title: "Fight Club", imageUrl: "https://www.tallengestore.com/cdn/shop/products/Fight_Club_-_Brad_Pitt_-_Ed_Norton_-_Hollywood_Cult_Classic_English_Movie_Poster_9a97cd80-ac93-4b18-9952-eb37b636a1c2.jpg?v=1591602945" },
+  { title: "Panchayat", imageUrl: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/8bd7fe142923441.6270c1e8e7ccc.jpg" },
   { title: "Mr Robot", imageUrl: "https://resizing.flixster.com/-XZAfHZM39UwaGJIFWKAE8fS0ak=/v3/t/assets/p14461667_b_v8_aa.jpg" },
   { title: "Flow", imageUrl: "https://image.tmdb.org/t/p/original/imKSymKBK7o73sajciEmndJoVkR.jpg" },
   { title: "Oppenheimer", imageUrl: "https://www.themoviedb.org/t/p/original/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg" },
 ];
 
-// Utility function to shuffle an array (Fisher-Yates shuffle algorithm)
+// Fisher-Yates shuffle algorithm)
 const shuffleArray = <T,>(array: T[]): T[] => {
   const newArray = [...array];
   for (let i = newArray.length - 1; i > 0; i--) {
